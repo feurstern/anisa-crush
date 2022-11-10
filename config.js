3d420374f81c881e14cbbd4df92044551928810a
feuerstern <rio-kurniawan@sakura-schutzstaffel.com>
2022-11-10 20:40:48 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "My crush",
+    introTitle: 'Hi Anisa, I want to say something ',
+    introDesc: `Btw, please explore the website and you have answer by your honest feeling and perhaps for better experience you have to wear earphone.`,
+    btnIntro: 'Next',
+    title: 'If I tell you who am I, do you wanna be my girlfriend?',
+    desc: 'pick yesss button pleaseeee!',
+    btnYes: 'Yessss',
+    btnNo: 'Nooo',
+    question: 'uhhhh so sweeettt! Btw do you have any comments that you want to send?',
+    btnReply: 'Next',
+    reply: '',
+    mess: 'Thanks for opening the website',
+    messDesc: 'Well, I did this becuase I have a feeling for you. I apologize if you think, this was cringe hehe. I love youuu, nissa',
+    btnAccept: 'ok',
+    messLink: './img/cuteCat.jpg' 
+};
+    
